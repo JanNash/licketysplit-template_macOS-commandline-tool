4ecae2d3081a6ae872eaa59e99a433a55c82f920
Jan Nash <jnash@jnash.de>
2019-02-15 00:54:02 +0100

    Add basic first version of commandline tool template

---
+++ PRODUCTNAME/PRODUCTNAME/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  PRODUCTNAME
+//
+//  Created by AUTHOR on 18.01.19.
+//  Copyright © 2019 ORGANIZATIONNAME. All rights reserved.
+//
+
+import Foundation
+
---
+++ PRODUCTNAME/PRODUCTNAMETests/PRODUCTNAMETests.swift
@@ -0,0 +1,40 @@
+//
+//  PRODUCTNAMETests.swift
+//  PRODUCTNAMETests
+//
+//  Created by Jan Nash on 21.01.19.
+//  Copyright © 2019 ORGANIZATIONNAME. All rights reserved.
+//
+
+import XCTest
+
+
+class PRODUCTNAMETests: XCTestCase {
+    
+    override class func setUp() {
+        
+    }
+    
+    override class func tearDown() {
+        
+    }
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+
+    func testExample() {
+        
+    }
+
+    func testPerformanceExample() {
+        self.measure {
+            
+        }
+    }
+
+}
